618ca58dab85ed93258945848b42df2bfeb10b00
Andreas Granér <andreas@graner.me>
2025-08-26 07:38:42 +0200

    WIP: searchParams

--- components/search.tsx
+++ components/search.tsx
@@ -1,13 +1,15 @@
-
+"use client"
 import { Input } from "@/components/ui/input"
-
+import { useSearchParams } from 'next/navigation'
 export default function Search() {
+    const searchParams = useSearchParams();
 
-    
+    const search = searchParams.get('search')
+
 
     return (
-        <section className="flex justify-center p-10">
+        <form className="flex justify-center p-10">
             <Input className="w-2xl"></Input>
-        </section>
+        </form>
     )
 }
