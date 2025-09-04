13e3934a95d2d9412b2e0beac6540478df0dbea0
Andreas Granér <Andreas@graner.me>
2025-09-04 11:55:30 +0200

    add footer

---
+++ components/footer.tsx
@@ -0,0 +1,11 @@
+export default function Footer() {
+  return (
+    <footer className="bg-[#2d2d2d] text-white full-width">
+      <div>
+        <h2>Pokédex </h2>
+      </div>
+      <p>explore the world of pokémon</p>
+      <nav></nav>
+    </footer>
+  );
+}
