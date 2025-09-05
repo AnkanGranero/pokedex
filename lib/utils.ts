c36fe00436c12ffa4a175423ada34eb8305e1e7f
Andreas Granér <Andreas@graner.me>
2025-09-05 13:26:47 +0200

    fix: change random number default limit to 1000

--- lib/utils.ts
+++ lib/utils.ts
@@ -6,7 +6,7 @@ export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
 
-export function randomId(min: number = 0, max: number = 1302): number {
+export function randomId(min: number = 0, max: number = 1000): number {
   return Math.floor(Math.random() * (max - min) + min);
 }
 
